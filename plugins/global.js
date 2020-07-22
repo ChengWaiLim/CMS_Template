08168ca096468cd8674be2a6ee022d2478dcbe3d
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-22 13:43:53 +0800

    updaate

--- plugins/global.js
+++ plugins/global.js
@@ -16,6 +16,17 @@ Vue.mixin({
         },
         cmsHeaderWidthWithCollapseNavigationBar(){
             return variables["cms-header-width-with-collapse-navigation-bar"]
+        },
+        ratio(){
+            if(screen.width < 480)
+                return variables["phone-width-ratio"]
+            else if (screen.width < 760)
+                return variables["small-pad-width-ratio"]
+            else if (screen.width < 960)
+                return variables["pad-width-ratio"]
+            else
+                return variables["pc-width-ratio"]
         }
-    }
+    },
+    
 })
